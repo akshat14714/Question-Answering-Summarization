df2bc44716ea8b107b90394d65fb589fe7cfcd3f
deep1010 <deepspammail10@gmail.com>
2019-02-09 13:21:09 +0530

    restructure optimized

--- restructureSingle.py
+++ restructureSingle.py
@@ -1,28 +1,27 @@
 import pandas as pd
 import numpy as np
-
+import pprint
 def loadFile(filename):
     csv_read = pd.read_csv(filename)
     return csv_read
     # print(csv_read)
 
 def restructrure(dataFrame):
-    myLis = list()
-    dataFrame = dataFrame.sort_values(by = ['QId'])
-    for _, row in dataFrame.iterrows():
-        if not any(d['QId']==row['QId'] for d in myLis):
-            dict = {'QId':row['QId'], 'QTitle':row['QTitle'], 'QBody':row['QBody'], 'Ans':[{'GId':row['GId'], 'GBody':row['GBody'], 'GScore':row['GScore']}], 'QTags':row['QTags']}
-            myLis.append(dict)
+    myLis = []
+    data = dataFrame.to_dict(orient='records')
+    prevId = '-1'
+    temp = {}
+    for row in data:
+        if row['QId'] != prevId:
+            prevId = row['QId']
+            if temp!= {}:
+                myLis.append(temp)
+            temp = {'QId':row['QId'], 'QTitle':row['QTitle'], 'QBody':row['QBody'], 'Ans':[{'GId':row['GId'], 'GBody':row['GBody'], 'GScore':row['GScore']}], 'QTags':row['QTags']}
         else:
-            for elem in myLis:
-                if elem['QId']==row['QId']:
-                    elem['Ans'].append({'GId':row['GId'], 'GBody':row['GBody'], 'GScore':row['GScore']})
-                    break
-
-        # print(myLis)
-    
+            temp['Ans'].append({'GId':row['GId'], 'GBody':row['GBody'], 'GScore':row['GScore']})
+        
     return myLis
 
-df = loadFile('QueryResultsCookingG5.csv')
+df = loadFile('qeury.csv')
 myLis = restructrure(df)
-print(myLis)
+pprint.pprint(myLis)
